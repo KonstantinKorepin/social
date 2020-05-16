ba6831e9351c76c69cda9beb1dce06597f4428c1
KonstantinKorepin <korepinwork@gmail.com>
2020-05-16 13:20:30 +0300

    Авторизация добавлена. Первый вариант.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PropertyInitializationFlawsInspection */
 
 namespace App\Http\Middleware;
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
